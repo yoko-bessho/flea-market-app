f997cdae1a497bd00664cb24a473245427b5fb96
yoko bessho <yoko.murakami.11.18@gmail.com>
2025-07-07 23:31:52 +0900

    migrationFile作成、postalCodeをchar７へ変更

---
+++ src/database/migrations/2025_07_07_093547_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedSmallInteger('price');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('is_sold', 20);
+            $table->string('image_path');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
