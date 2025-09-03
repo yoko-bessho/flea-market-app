27da87d0d200816b96807f77e90d5673f18f3356
yoko bessho <yoko.murakami.11.18@gmail.com>
2025-09-03 17:16:44 +0900

    会員登録機能のテスト

--- src/tests/Feature/FleaMarketTest.php
+++ src/tests/Feature/FleaMarketTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class FleaMarketTest extends TestCase
 {
@@ -18,7 +20,6 @@ class FleaMarketTest extends TestCase
      */
     public function test_register_userName_validationMessage()
     {
-
         app()->setLocale('ja');
 
         $response = $this->get('/register');
@@ -32,6 +33,106 @@ class FleaMarketTest extends TestCase
         ]);
 
         $response->assertSessionHasErrors(['name' => 'お名前を入力してください。',
-    ]);
+        ]);
     }
+
+    public function test_register_userEmail_validationMessage()
+    {
+        app()->setLocale('ja');
+
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+
+        $response = $this->post('/register', [
+            'name' => 'testName',
+            'email' => '',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertSessionHasErrors(['email' => 'メールアドレスを入力してください。',
+        ]);
+    }
+
+    public function test_register_userPassword_validationMessage()
+    {
+        app()->setLocale('ja');
+
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+
+        $response = $this->post('/register', [
+            'name' => 'testName',
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => '',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertSessionHasErrors(['password' => 'パスワードを入力してください。',
+        ]);
+    }
+
+    public function test_register_userPassword_too_short_validationMessage()
+    {
+        app()->setLocale('ja');
+
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+
+        $response = $this->post('/register', [
+            'name' => 'testName',
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => '1234567',
+            'password_confirmation' => '1234567',
+        ]);
+
+        $response->assertSessionHasErrors(['password' => 'パスワードは8文字以上で入力してください。',
+        ]);
+    }
+
+    public function test_register_userPasswordConfirmation_validationMessage()
+    {
+        app()->setLocale('ja');
+
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+
+        $response = $this->post('/register', [
+            'name' => 'testName',
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => '1234567',
+            'password_confirmation' => '1234568',
+        ]);
+
+        $response->assertSessionHasErrors(['password' => 'パスワードと一致しません。',
+        ]);
+    }
+
+    public function test_register_user_success_redirects_to_profilePage()
+    {
+        app()->setLocale('ja');
+
+        $email = $this->faker->unique()->safeEmail();
+
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+
+        $response = $this->post('/register', [
+            'name' => 'testName',
+            'email' => $email,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $this->assertDatabaseHas('users',[
+            'name'=>'testName',
+            'email'=> $email,
+        ]);
+
+        $user = User::where('email', $email)->first();
+        $this->assertTrue(Hash::check('password', $user->password));
+
+        $response->assertRedirect(route('setProfile'));
+    }
+
 }
