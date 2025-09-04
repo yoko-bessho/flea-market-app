827529fc95296697c2c6c3a83ab801488b52fe1e
yoko bessho <yoko.murakami.11.18@gmail.com>
2025-09-04 16:14:56 +0900

    ログイン機能テスト

--- src/tests/Feature/FleaMarketTest.php
+++ src/tests/Feature/FleaMarketTest.php
@@ -18,6 +18,8 @@ class FleaMarketTest extends TestCase
      *
      * @return void
      */
+
+// 会員登録画面
     public function test_register_userName_validationMessage()
     {
         app()->setLocale('ja');
@@ -135,4 +137,92 @@ class FleaMarketTest extends TestCase
         $response->assertRedirect(route('setProfile'));
     }
 
+
+// ログイン機能
+    public function test_login_userEmail_validationMessage()
+    {
+        app()->setLocale('ja');
+
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $response = $this->post('/login', [
+            'email' => '',
+            'password' => 'password',
+        ]);
+
+        $response->assertSessionHasErrors(['email' => 'メールアドレスを入力してください。',
+        ]);
+    }
+
+    public function test_login_userPassword_validationMessage()
+    {
+        app()->setLocale('ja');
+
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $response = $this->post('/login', [
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => '',
+        ]);
+
+        $response->assertSessionHasErrors(['password' => 'パスワードを入力してください。',
+        ]);
+    }
+
+    public function test_login_userEmail_wrong_validationMessage()
+    {
+        app()->setLocale('ja');
+
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $response = $this->post('/login', [
+            'email' => 'notexit' . time() . '@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertSessionHasErrors(['email' => 'ログイン情報が登録されていません。',
+        ]);
+    }
+
+    public function test_login_userPassword_wrong_validationMessage()
+    {
+        app()->setLocale('ja');
+
+        $user = User::factory()->create([
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'),
+        ]);
+
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrong_password',
+        ]);
+
+        $response->assertSessionHasErrors(['email' => 'ログイン情報が登録されていません。',
+        ]);
+    }
+
+    public function test_login_success()
+    {
+        $user = User::factory()->create([
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'),
+        ]);
+
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        $response = $this->post('login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticatedAs($user);
+    }
 }
